07293186e4e64e2fc694cdcf6ca88d618b7092f2
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-03-12 20:09:29 +0530

    sidebar working

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,19 +1,26 @@
 import React from "react";
+
 import { CiViewTable } from "react-icons/ci";
 import { CiCreditCard1 } from "react-icons/ci";
 
-const Sidebar = () => {
+const Sidebar = ({ showType, setShowType }) => {
   return (
     <div className="bg-[#F1F1F1] border-r border-zinc-300 w-110 p-4">
       <p className="text-xl font-semibold mb-20">Book List</p>
       <div className="flex flex-col gap-3">
         <p className="font-semibold mx-3">Menu</p>
         <ul className="flex flex-col">
-          <li className="sidebarItems bg-white">
+          <li
+            className={`sidebarItems ${showType == "table" ? "bg-white" : ""}`}
+            onClick={() => setShowType("table")}
+          >
             <CiViewTable className="sidebarIcons" />
             Table
           </li>
-          <li className="sidebarItems">
+          <li
+            className={`sidebarItems ${showType == "card" ? "bg-white" : ""}`}
+            onClick={() => setShowType("card")}
+          >
             <CiCreditCard1 className="sidebarIcons" />
             Card
           </li>
