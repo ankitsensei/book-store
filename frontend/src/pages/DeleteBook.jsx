ef51a5dfa7f5b4b566fa6b01c4f0cab5ec917492
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-03-21 10:18:39 +0530

    ui improved

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -26,15 +26,15 @@ const DeleteBook = () => {
       });
   };
   return (
-    <div className="p-4">
+    <div className="p-4 w-full h-screen bg-[#F1F1F1]">
       <BackBtn />
       <h1 className="text-3xl my-4">Delete Book</h1>
 
       {loading ? <Spinner /> : ""}
-      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-150 p-4 mx-auto">
+      <div className="flex flex-col justify-center items-center bg-white rounded-xl w-150 p-4 mx-auto">
         <p>Are you sure you want to delete this book?</p>
         <button
-          className="bg-red-500 text-white p-2 my-4"
+          className="bg-red-500 text-white px-4 py-2 rounded-lg mt-4"
           onClick={() => handleDeleteBook()}
         >
           Yes, delete it
