17b3df79d1baf315ac961b4e0a73d448c9fdc4c0
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-05-09 23:43:54 +0530

    style: sidebar h-screen

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import { CiCreditCard1 } from "react-icons/ci";
 
 const Sidebar = () => {
   return (
-    <div className="bg-[#F1F1F1] border-r border-zinc-300 w-110 h-full p-4 hidden lg:block">
+    <div className="bg-[#F1F1F1] border-r border-zinc-300 w-110 h-screen p-4 hidden lg:block">
       <p className="text-xl font-semibold mb-20">Book List</p>
       <div className="flex flex-col gap-3">
         <p className="font-semibold mx-3">Menu</p>
